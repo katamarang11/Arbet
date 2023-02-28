2abf8e73a42bb347095a4f65dda02e74a1e6f09c
katamarang11 <arestov.maks@mail.ru>
2023-02-28 16:25:48 +0300

    commit

--- js/index/script-scroll.js
+++ js/index/script-scroll.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
   const navOffset = $('#nav').offset().top;
   const cardsOffset = $('.collection_background').offset().top;
+  const advantagesTitle = $('.advantages__title').offset().top;
+
   $(window).scroll(function () {
     const scrolled = $(this).scrollTop();
+    // console.log(scrolled);
+    console.log(advantagesTitle);
 
     if (scrolled > navOffset) {
       // шапка прилипла
@@ -17,6 +21,10 @@ $(document).ready(function () {
       $('.card_animation-2').addClass('animation-2');
       $('.card_animation-3').addClass('animation-3');
       $('.card_animation-4').addClass('animation-4');
+    }
+
+    if (scrolled > advantagesTitle - 200) {
+      $('.advantages-less').addClass('advantages-more');
     }
   });
 });
